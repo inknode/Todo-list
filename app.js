5e605eed5faa7d4f02c5d184d51e84e9c518ccb9
mygit <mygit@test.com>
2020-08-16 15:05:46 +0100

    minor fixes

--- app.js
+++ app.js
@@ -15,26 +15,29 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(__dirname + "/public"));
 
-mongoose.connect("mongodb://localhost:27017/Todo", {useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb://localhost:27017/Todo", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-const itemSchema ={
-  name:String
-};
+const itemSchema = new mongoose.Schema({
+  name: String,
+});
 const Item = mongoose.model("Item", itemSchema);
 
 const item1 = new Item({
-  name: "GYM"
+  name: "GYM",
 });
 
 const item2 = new Item({
-  name: "Play Games"
+  name: "Play Games",
 });
 
 const item3 = new Item({
-  name: "Buy Food"
+  name: "Buy Food",
 });
 
-const defaultItem = [ item1,item2,item3];
+const defaultItem = [item1, item2, item3];
 
 /* app looks at the root directory, uses dayjs to format date and renders using ejs
 also storing default items to db */
@@ -43,18 +46,22 @@ app.get("/", (req, res) => {
   const day = dayjs().format("MMMM D, YYYY");
   const copyrightDate = dayjs().format("YYYY");
 
-  Item.find({},(err, defaultItems)=>{
-    if(defaultItems.length === 0){
-      Item.insertMany(defaultItem,(err)=>{
-        if(err){
-          console.log(err)
-        }else{
-          console.log("successfully saved default items to DB")
+  Item.find({}, (err, defaultItems) => {
+    if (defaultItems.length === 0) {
+      Item.insertMany(defaultItem, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("successfully saved default items to DB");
         }
       });
-      res.redirect("/")
-    }else{
-    res.render("todo", { date: day, newlist: defaultItems, year: copyrightDate });
+      res.redirect("/");
+    } else {
+      res.render("todo", {
+        date: day,
+        newlist: defaultItems,
+        year: copyrightDate,
+      });
     }
   });
 });
@@ -62,20 +69,19 @@ app.get("/", (req, res) => {
 /*Add and delete items from DB, using body parser
 to grab user inputs from views form*/
 app.post("/", (req, res) => {
-  const item = req.body.newItem;
   const addItem = new Item({
-     name: item
+    name: req.body.newItem
   });
- addItem.save();
- console.log("items added succesfully");
- res.redirect("/");
+  addItem.save();
+  console.log("items added succesfully");
+  res.redirect("/");
 });
 
-app.post("/delete", (req, res)=>{
-  const deleteItem = req.body.checkbox
-  Item.findByIdAndRemove(deleteItem,(err)=>{
-    if(!err){
-      console.log('items deleted successfully');
+app.post("/delete", (req, res) => {
+  const deleteItem = req.body.checkbox;
+  Item.findByIdAndRemove(deleteItem, (err) => {
+    if (!err) {
+      console.log("items deleted successfully");
       res.redirect("/");
     }
   });
